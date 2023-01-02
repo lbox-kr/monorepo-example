0a1fa7c4bb487f74eec538c2b23a4fdc58e5c7a1
Yoon Han <yoontopia94@gmail.com>
2023-01-02 21:04:24 +0900

    chore: eslint typescript parser 추가

--- packages/storybook/.eslintrc.js
+++ packages/storybook/.eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2015,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   extends: ['plugin:storybook/recommended'],
   rules: {},
 }

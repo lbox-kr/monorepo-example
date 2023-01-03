a535906d4ac75fc0100e84aa329a1732b6124098
Yoon Han <yoontopia94@gmail.com>
2023-01-03 10:11:32 +0900

    chore: lint-staged 에 Prettier script 추가

--- apps/nextjs/.lintstagedrc.js
+++ apps/nextjs/.lintstagedrc.js
@@ -1,5 +1,3 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [
-    'yarn lint:nextjs',
-  ]
+  '*.{js,jsx,ts,tsx}': ['yarn lint:nextjs', 'yarn prettier:nextjs'],
 }
--- packages/storybook/.lintstagedrc.js
+++ packages/storybook/.lintstagedrc.js
@@ -1,5 +1,3 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [
-    'yarn lint:storybook',
-  ]
+  '*.{js,jsx,ts,tsx}': ['yarn lint:storybook', 'yarn prettier:storybook'],
 }
